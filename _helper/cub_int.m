b3a29698a3bc2b73ca4aff9367df19528c810f34
elliotpatros <empatros@gmail.com>
2016-10-11 12:10:22 -0700

    it works. can interpolate between two 'hills'

--- _helper/cub_int.m
+++ _helper/cub_int.m
@@ -1,41 +1,26 @@
 function y = cub_int(array, position)
 
-%% do input error checking
 L = length(array);
-if L < 4
-%     error('length of array must be greater than 4');
-    temp = zeros(4, 1);
-    scale = L / 4;
-    for n = 1:L
-        temp(n) = lin_int(array, n * scale);
-    end
-    L = 4;
-    array = temp;
-    clear temp;
+if L < 2
+    % do input error checking
+    y = array(1);
+elseif L < 4
+    % do linear interpolation if array is too small for cubic
+    y = lin_int(array, position);
+else
+    % make index 0
+    I = floor(position);
+    D = position - I;
+
+    % lookup values
+    X = array(min(max([I-1, I, I+1, I+2], 1), L));
+    a = (3 * (X(2) - X(3)) - X(1) + X(4)) / 2;
+    b = 2 * X(3) + X(1) - (5 * X(2) + X(4)) / 2;
+    c = (X(3) - X(1)) / 2;
+    
+    % solve
+    y = (((a * D) + b) * D + c) * D + X(2);
 end
-
-%% make index 0
-I = zeros(4, 1);
-I(2) = floor(position);
-dec = position - I(2);
-
-%% make indexes
-I(1) = I(2) - 1;
-I(3) = I(2) + 1;
-I(4) = I(2) + 2;
-
-I(I<1) = 1;
-I(I>L) = L;
-
-%% lookup values
-X = array(I);
-a = (3*(X(2)-X(3))-X(1)+X(4))/2;
-b = 2*X(3)+X(1)-(5*X(2)+X(4))/2;
-c = (X(3)-X(1))/2;
-
-%% solve
-y = (((a*dec)+b)*dec+c)*dec+X(2);
-
 
 end
 
--- test.m
+++ test.m
@@ -3,27 +3,23 @@ clearvars;
 addpath(genpath('.'));
 
 %% weight
-weight = 0.5;
+for weight = linspace(0, 1, 1000)
 
-%% peaks
-A = [0 1 2 3 4 5 4 3 2 1 0];
-B = [0 2 3 1 0];
-
-%% weights
-Aweight = 1 - weight;
-Bweight = weight;
+%% fake peaks
+A = [linspace(-4, 10, 200) linspace(9, 1, 100)];
+B = [linspace(2, 11, 5) linspace(12, -1, 8)];
 
 %% lengths
 LA = length(A);
 LB = length(B);
-newL = round(Aweight * LA + Bweight * LB);
+newL = round(weighted_mean(LA, LB, weight));
 newPeak = zeros(newL, 1);
 
 %% maxima
 [maxA, maxAi] = max(A);
 [maxB, maxBi] = max(B);
-newMax = Aweight * maxA + Bweight * maxB;
-newMaxi = round(Aweight * maxAi + Aweight * maxBi);
+newMax = weighted_mean(maxA, maxB, weight);
+newMaxi = round(weighted_mean(maxAi, maxBi, weight));
 
 %% interpolate min and max
 newPeak(1) = weighted_mean(A(1), B(1), weight);
@@ -31,29 +27,29 @@ newPeak(newL) = weighted_mean(A(end), B(end), weight);
 newPeak(newMaxi) = weighted_mean(A(maxAi), B(maxBi), weight);
 
 %% upward slope
-Aup = A(1:maxAi);
-Bup = B(1:maxBi);
+Aup = A(2:maxAi-1);
+Bup = B(2:maxBi-1);
 from = 2;
 thru = newMaxi-1;
-Ascale = (length(Aup) - 1) / length(from:thru);
-Bscale = (length(Bup) - 1) / length(from:thru);
+Ascale = length(Aup) / length(from:thru);
+Bscale = length(Bup) / length(from:thru);
 for n = from:thru
-    pos = (n + 1) - from;
+    pos = (n - from) + 1;
     An = cub_int(Aup, pos * Ascale);
     Bn = cub_int(Bup, pos * Bscale);
     newPeak(n) = weighted_mean(An, Bn, weight);
 end
 
 %% downward slope
-Adn = A(maxAi:end);
-Bdn = B(maxBi:end);
+Adn = A(maxAi+1:end-1);
+Bdn = B(maxBi+1:end-1);
 from = newMaxi + 1;
 thru = newL - 1;
-Ascale = (length(Adn) - 1) / length(from:thru);
-Bscale = (length(Bdn) - 1) / length(from:thru);
+Ascale = length(Adn) / length(from:thru);
+Bscale = length(Bdn) / length(from:thru);
 
 for n = from:thru
-    pos = (n + 1) - from;
+    pos = (n - from) + 1;
     An = cub_int(Adn, pos * Ascale);
     Bn = cub_int(Bdn, pos * Bscale);
     newPeak(n) = weighted_mean(An, Bn, weight);
@@ -68,6 +64,7 @@ toplot(2, 1:LB) = B;
 toplot(3, 1:newL) = newPeak;
 plot(toplot')
 
-% subplot(311); plot(A);
-% subplot(312); plot(B);
-% subplot(313); plot(newPeak);
+drawnow;
+% pause(0.01)
+
+end
