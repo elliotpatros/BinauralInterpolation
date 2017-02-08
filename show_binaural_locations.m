d414b5dc4855d1da78c2b9fbe3ea02d6be35c451
Elliot Patros <epatros@ucsd.edu>
2017-02-08 13:13:45 -0800

    fixed graph of hrir locations

--- show_binaural_locations.m
+++ show_binaural_locations.m
@@ -1,6 +1,6 @@
 %% user parameters
 subj = 1006;
-dist = 1;
+radius = 1;
 
 %% load resources
 load(['./Resources/IRC_', num2str(subj), '_C_HRIR.mat']);
@@ -11,13 +11,14 @@ if (length(r_eq_hrir_S.elev_v) ~= nPositions)
     error(['the hrtf library, ', num2str(subj), ' is fubar']);
 end
 
-for n = 1:nPositions
-    azim = l_eq_hrir_S.azim_v(n);
-    elev = l_eq_hrir_S.elev_v(n);
-    disp(['n = ', num2str([azim, elev, dist])]);
-    [x, y, z] = sph2cart(azim, elev, dist);
-    plot3(x, y, z, 'rx');
-    hold on;
-end
+azim = deg2rad(l_eq_hrir_S.azim_v);
+elev = deg2rad(l_eq_hrir_S.elev_v);
+dist = ones(size(azim)) .* radius;
 
+plot3(0, 0, 0, 'bo');
+grid on;
+hold on;
+
+[x,y,z] = sph2cart(azim, elev, dist);
+plot3(x, y, z, 'ro');
 hold off;
